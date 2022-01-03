e39fed04e3b199a716de25c5782a6cbde24f0814
HÜMEYRA ÇİMEN <75569106+hmyrcmn@users.noreply.github.com>
2022-01-03 23:14:17 +0300

    infix prefixpostfix calculater using with bts in c programing
    Aşağıda verilen ikili ağacı yaratın;
     Ağacın infix, prefix ve postfix ifadelerini ekrana
    basmak için bir menü seçimi oluşturun.
     Ağacı seçilen ifade türüne göre ekrana bastırın.

---
+++ bst_examples/main.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node {
+    char data;
+    struct node* left;
+    struct node* right;
+
+}NODE;
+
+NODE* add(char data) {
+    // new_node eklenecek nodumm
+    struct node* new_node;
+    new_node=(struct node* )malloc(sizeof(struct node));
+    new_node->data=data;
+    new_node->left=NULL;
+    new_node->right=NULL;
+     return new_node;
+};
+void inr(NODE* p)
+{
+    if (p == NULL) {
+        return;
+    }
+    else {
+        inr(p->left);
+        printf("%c ", p->data);
+        inr(p->right);
+    }
+}
+void postr(NODE* p)
+{
+    if (p == NULL) {
+        return;
+    }
+    else {
+        postr(p->left);
+        postr(p->right);
+        printf("%c ", p->data);
+    }
+}
+void pre(NODE* p){
+    if (p == NULL) {
+        return;
+    }
+    else{
+         printf("%c ", p->data);
+         pre(p->left);
+         pre(p->right);
+    }
+}
+int main()
+{
+        NODE* kok= add('+');
+        kok->left=add('*');
+        kok->left->left=add('c');
+
+        kok->left ->right=add('+');
+        kok->left ->right->left=add('a');
+        kok->left ->right->right=add('b');
+
+        kok->right = add('*');
+        kok->right->left = add('d');
+        kok->right->right= add('*');
+        kok->right->right->left=add('f');
+        kok->right->right->right=add('+');
+        kok->right->right->right->left=add('g');
+        kok->right->right->right->right=add('h');
+
+        printf("inr: \n");
+        inr(kok);
+        printf("\n");
+        printf("postr: \n");
+        postr(kok);
+        printf("\n");
+        printf("pre: \n");
+        pre(kok);
+
+
+    return 0;
+}
